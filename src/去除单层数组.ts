e69334fa3755017a4e052bea9216475ebaf8f91b
hbybyyang <2451759073@qq.com>
2021-12-02 03:58:31 +0800

    判定双层数组

--- src/去除单层数组.ts
+++ src/去除单层数组.ts
@@ -1,1 +1,8 @@
+import { 类型等价判定 } from './类型等价判定'
+
 export type 去除单层数组<Arr extends any[]> = Arr extends [infer a] ? a : Arr
+
+var a1: 类型等价判定<去除单层数组<[1]>, 1> = true
+var a2: 类型等价判定<去除单层数组<[[1]]>, [1]> = true
+var a3: 类型等价判定<去除单层数组<[[1, 2]]>, [1, 2]> = true
+var a4: 类型等价判定<去除单层数组<[[1], 3]>, [[1], 3]> = true
