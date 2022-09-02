f6e6e8c4bfac80e7276a7ee8bd35aa02e525180e
hbybyyang <2451759073@qq.com>
2022-09-02 14:24:31 +0800

    .

--- src/字符串以某值开头.ts
+++ src/字符串以某值开头.ts
@@ -1,3 +1,3 @@
 import { error } from './error'
 
-export type 以s开头<s extends string, A> = A extends `${s}${infer a}` ? true : error<[A, `必须以${s}开头`]>
+export type 字符串以某值开头<s extends string, A> = A extends `${s}${infer a}` ? true : error<[A, `必须以${s}开头`]>
